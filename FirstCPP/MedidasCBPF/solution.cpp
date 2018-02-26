2d87091ffacc7bcd8e5e0770b291c2a15c697b6f
luis-pinheres <pinheresluis@gmail.com>
2018-02-26 15:03:34 -0300

    Command Line

--- FirstCPP/MedidasCBPF/solution.cpp
+++ FirstCPP/MedidasCBPF/solution.cpp
@@ -13,18 +13,19 @@
 #include "TAxis.h"
 
 
-using namespace std;
+ using namespace std;
 
 
-int main(int argc, char* argv[])
-{
-  const int samples = 100;
-  if (argc >1){
+  int main(int argc, char* argv[])
+  {
+   const int samples = 100;
+   
+   if (argc >1){
    cout<<"argv[1]="<<argv[1]<<endl;   
-  } else{
+   } else{
    cout<<" No file name entered. Exiting..."<<endl;
    return -1;
-  }
+   }
  
     // open file to read
     ifstream inFile(argv[1]);
@@ -38,15 +39,15 @@ int main(int argc, char* argv[])
      while(getline(inFile,aLine)){
     //  cout<<aLine<<'\n';
      }
-     
     }  
-      else {
-      cout << "Failed to open file.."<<endl;
-      }
+    
+    else {
+         cout << "Failed to open file.."<<endl;
+         }
     
 
 
-// Columnas        
+   // Columnas        
    int  meas[samples];
    int col2[samples];
    int col3[samples];
@@ -55,15 +56,15 @@ int main(int argc, char* argv[])
    // k lines(6477000)
 
 
-  // skip first 11 lines
-  string Line;
-  int n=11;
-  for( int i =0; i<n; ++i){
-  getline(inFile,Line);
-  cout << Line << endl;
-  }
+   // skip first 11 lines
+   string Line;
+   int n=11;
+   for( int i =0; i<n; ++i){
+   getline(inFile,Line);
+   cout << Line << endl;
+   }
 
-  // Plot 100 datas   
+   // Plot 100 datas   
                                   
     for( int i = 0; i < samples ; ++i){
     inFile >> dump>> dump >> dump >> meas[i] //
@@ -71,25 +72,20 @@ int main(int argc, char* argv[])
      m[i]=25*i;
                                                                                                                            
     cout <<" * "<<" *  "<<" * "<< meas[i] <<endl;
-   // cout<<"--"<<col2[i]<<"--"<<col3[i]<<"******"<< endl;
+    // cout<<"--"<<col2[i]<<"--"<<col3[i]<<"******"<< endl;
                                                                                                 
-    }                                                                                                                                                    TCanvas*c1= new TCanvas("c1","A",200,10,700,500);                                                                                                                                                                                                                                                           TGraph g(samples,m,meas);
-   g.GetYaxis()->SetTitle("adc count");
-                                                                                                                                                                                                                                                                                                               g.Draw();
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 c1->Print("CPE1.pdf","pdf");
-  //                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
-  //                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
-  //                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        //skip 2 lines
-  //                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        for(int i=0; i<3; ++i){
-string aLines;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           getline(inFile,aLines);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            cout<<aLines<<endl;                                                                                                                                                                                                                                            cout<<"_________________"<<endl;   
-}
-
-
-
-
-
-
+    }                                                                                                                                                    TCanvas*c1= new TCanvas("c1","A",200,10,700,500);                                                                                                     TGraph g(samples,m,meas); 
+   gStyle->SetCanvasColor(0);
+   gStyle->SetFrameBorderMode(0);
+   gStyle->SetStatBorderSize(1);
+   gStyle->SetFrameFillColor(0);
+   gStyle->SetTitleFillColor(0);                   
+                    
+   g.SetLineColor(3);
+   g.SetLineWidth(2);
+   g.SetTitle("Channel-Plate-Event");
+   g.GetXaxis()->SetTitle("t [ns]");
+   g.GetYaxis()->SetTitle("adc count");                                                                                                                  g.Draw();                                                                                                                                             c1->Print("CPE1.pdf","pdf");                                                                                                                                                                                                                                                                                                                                                                                                                               
       return 0;
   }  
 
