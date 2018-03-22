7b01f310262279e6d41e917264c398a692acdf50
luis-pinheres <pinheresluis@gmail.com>
2018-03-22 11:32:43 -0300

    Example

--- FirstCPP/MedidasCBPF/HistogramLegend.cpp
+++ FirstCPP/MedidasCBPF/HistogramLegend.cpp
@@ -1,4 +1,11 @@
-#include <fstream> //for file acess
+//Codigo donde graficamos un Histograma y una funcion al mismo tiempo.
+//Resuelto por: Luis F. Pineres Rico
+//Fecha:5-03-2018
+
+
+
+//Definimos las librerias
+#include <fstream> 
 #include <iostream>
 #include <string>
 #include <vector>
@@ -13,47 +20,54 @@
 #include "TArrow.h"
 #include "TH1F.h"
 #include "TF1.h"
- using namespace std;
+ 
+
+
+
+  using namespace std;
 
 
   int main()
   {
-  TCanvas *c = new TCanvas("c","c",600,500);
-   gStyle->SetOptStat(0);
-   TH1F* h1 = new TH1F("h1","TLegend Example",200,-10,10);
-   h1->FillRandom("gaus",30000);
-   h1->SetFillColor(0);
+   
+   //Definimos nuestro espacio 
+   TCanvas *c = new TCanvas(); 
+   gStyle->SetOptStat(111111);
+   
+   // Creamos nuestro primer Histogramas
+   TH1F* h1 = new TH1F("h1","Data Analysis",400,-10,10);
+   
+   // Definimos nuestra distribución de tipo Gaussiana
+   h1->FillRandom("gaus",300000);
+   
+   // Detalles de estetica para el grafico
+   h1->SetFillColor(7);
    h1->SetFillStyle(3003);
+   h1->SetMarkerColor(4);
+   h1->SetMarkerStyle(25);
+   h1->SetLineStyle(9);
+   h1->GetXaxis()->SetTitle("Gaussian");
+   h1->SetLineColor(kYellow);
+   h1->Fit("gaus");
    h1->Draw();
-   TF1 *f1=new TF1("f1","1000*TMath::Abs(sin(x)/x)",-10,10);
-   f1->SetLineColor(kBlue);
-   f1->SetLineWidth(4);
-   f1->Draw("same");
-   const int n = 20;
-   double x[n], y[n], ex[n], ey[n];
-   for (int i=0;i<n;i++) {
-      x[i]  = i*0.1;
-      y[i]  = 1000*sin(x[i]+0.2);
-      x[i]  = 17.8*x[i]-8.9;
-      ex[i] = 1.0;
-      ey[i] = 10.*i;
-   }
-   TGraphErrors *gr = new TGraphErrors(n,x,y,ex,ey);
-   gr->SetName("gr");
-   gr->SetLineColor(kRed);
-   gr->SetLineWidth(2);
-   gr->SetMarkerStyle(21);
-   gr->SetMarkerSize(1.3);
-   gr->SetMarkerColor(7);
-   gr->Draw("P");
-   TLegend* leg = new TLegend(0.1,0.7,0.48,0.9);
-   leg->SetHeader("The Legend Title");
-   leg->AddEntry(h1,"Histogram filled with random numbers","f");
-   leg->AddEntry("f1","Function abs(#frac{sin(x)}{x})","l");
-   leg->AddEntry("gr","Graph with error bars","lep");
-   leg->Draw();
-   c->Print("GRAFICA.pdf","pdf");
 
+   // Creamos una función y los detalles del grafico
+   TF1 *f1=new TF1("f1","1000*TMath::Abs((sin(x)/x))",-10,10);
+   f1->SetLineColor(kBlue);
+   f1->SetLineWidth(1);
+   f1->Draw("same");
+
+   // Leyenda del grafico
+   TLegend* leg = new TLegend(0.12,0.7,0.3,0.89);
+   leg->SetHeader("The Legend Title");
+   leg->AddEntry(h1,"Histogram Gaussian Distribution","f");
+   leg->AddEntry("f1","f(x)=abs(sinx/x)","l");
+   leg->SetFillColor(0);
+   leg->Draw();
+
+   // guardamos nuestro grafico como  .pdf
+   c->Print("GRAFICA.pdf","pdf");
+   
    return 0;
 
 }
